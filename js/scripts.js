3271433290a394006315b318d326a73245e76675
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-02-25 14:05:49 -0800

    editing send email

--- js/scripts.js
+++ js/scripts.js
@@ -159,10 +159,12 @@ $(window).on("load",function (){
 
 
     // contact form validator
+    $('#contact-form').validator()
     $('#contact-form').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
+        e.preventDefault();
+        // if (!e.isDefaultPrevented()) {
             var url = "contact.php";
-
+            $('#sendButton').text('Sending')
             $.ajax({
                 type: "POST",
                 url: url,
@@ -176,11 +178,46 @@ $(window).on("load",function (){
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
                         $('#contact-form')[0].reset();
+                        $('#sendButton').text('Sent')
                     }
+                },
+                error: function(err){
+                    var alertBox = '<div class="alert alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+ "Unable to Send Email" + '</div>';
+                    $('#contact-form').find('.messages').html(alertBox);
+                    $('#sendButton').text('Sent')
                 }
             });
             return false;
-        }
+        // }
     });
+
+
+
+
+
+
+    // $('#contact-form').on('submit', function (e) {
+    //     if (!e.isDefaultPrevented()) {
+    //         var url = "contact.php";
+
+    //         $.ajax({
+    //             type: "POST",
+    //             url: url,
+    //             data: $(this).serialize(),
+    //             success: function (data)
+    //             {
+    //                 var messageAlert = 'alert-' + data.type;
+    //                 var messageText = data.message;
+
+    //                 var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+    //                 if (messageAlert && messageText) {
+    //                     $('#contact-form').find('.messages').html(alertBox);
+    //                     $('#contact-form')[0].reset();
+    //                 }
+    //             }
+    //         });
+    //         return false;
+    //     }
+    // });
 
 });
